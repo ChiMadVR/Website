3876b34844d26280355ca26b4a67c67c72789201
badsnyper <solovy.sam@gmail.com>
2016-10-26 23:50:12 -0500

    Added gitignore file and updated gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,32 @@
+'use strict';
 var gulp = require("gulp"),
 	runSequence = require('run-sequence'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify');
-	paths = {
-		destination: "dist/"
-	},
-	files = {
-		minJavascript: [
-			"!app/scripts/**/*spec.js",
-			"app/scripts/**/*.js"
-		]
+	uglify = require('gulp-uglify'),
+	sass = require('gulp-sass');
+
+var paths = {
+	destination: "dist/",
+
+	files: {
+		minJavascript: [ "!app/scripts/**/*spec.js",
+							"app/scripts/**/*.js" ],
+		stylesheets: "stylesheets/*.scss"
 	}
+}
 
 gulp.task("build", function(cb) {
-	runSequence(["build-css","build-js"], cb);
+	runSequence(["build-sass","build-js"], cb);
 });
 
-gulp.task("build-css", function(){
-	gulp.src("css/*.css")	
+gulp.task("build-sass", function(){
+	return gulp.src(paths.files.stylesheets)
+	.pipe(sass({outputSyle: 'compressed'}).on('error',sass.logError))
 	.pipe(gulp.dest(paths.destination));
 });
 
 gulp.task("build-js", function(){
-	gulp.src(files.minJavascript)
+	return gulp.src(paths.files.minJavascript)
 	.pipe(uglify())
 	.pipe(concat("javascript.min.js"))
 	.pipe(gulp.dest(paths.destination));
